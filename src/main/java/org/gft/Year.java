27c63c9fc96854d5a2109ee0c2b0d51cb6af8593
annt <annt@gft.com>
2022-11-17 16:24:37 +0000

    .

--- src/main/java/org/gft/Year.java
+++ src/main/java/org/gft/Year.java
@@ -1,2 +1,25 @@
-package org.gft;public class Year {
+package org.gft;
+
+public class Year {
+
+    private int year;
+
+    public Year(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    boolean isLeap(){
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            return true;
+        }else
+            return false;
+    }
 }
--- src/test/java/org/gft/LeapYearTest.java
+++ src/test/java/org/gft/LeapYearTest.java
@@ -1,7 +1,44 @@
 package org.gft;
+
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class Test {
+import static org.junit.jupiter.api.Assertions.*;
 
-@org.junit.jupiter.api.Test
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LeapYearTest {
+
+
+    @ParameterizedTest(name = "Run: {index}  -  value: {arguments}")
+    @ValueSource(ints = {5832,392,2528})
+    void LeapYearDivisibleBy4(int years){
+        Year year = new Year(years);
+
+        assertTrue(year.isLeap());
+    }
+
+    @ParameterizedTest(name = "Run: {index}  -  Year: {arguments}")
+    @ValueSource(ints = {333,6537,1254})
+    void LeapYearNotDivisibleBy4(int years){
+        Year year = new Year(years);
+
+        assertFalse(year.isLeap());
+    }
+
+    @ParameterizedTest(name = "Run: {index}  -  value: {arguments}")
+    @ValueSource(ints = {400,1200,2800})
+    void LeapYearDivisibleBy400(int years){
+        Year year = new Year(years);
+
+        assertTrue(year.isLeap());
+    }
+
+    @ParameterizedTest(name = "Run: {index}  -  value: {arguments}")
+    @ValueSource(ints = {200,1000,9000})
+    void LeapYearNotDivisibleBy400(int years){
+        Year year = new Year(years);
+
+        assertFalse(year.isLeap());
+    }
 }
